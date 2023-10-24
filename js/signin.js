a775b307843a1a93829347907e7d1672909df51f
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-10-24 14:16:53 +0530

    new:added the functionality of the sign in and sign up

---
+++ js/signin.js
@@ -0,0 +1,81 @@
+function signIn() {
+  let email = document.getElementById("login-email").value;
+  let password = document.getElementById("login-password").value;
+
+  let storedUser = localStorage.getItem(email);
+  if (storedUser) {
+    let user = JSON.parse(storedUser);
+    if (user.password === password) {
+      showSuccessMessage("Login successful!");
+      let ema = document.getElementById("login-email");
+      let pass = document.getElementById("login-password");
+      ema.value = "";
+      pass.value = "";
+      document.getElementById("login-error").style.display = "none";
+      window.location.href = "./home.html";
+      // Hide error message
+    } else {
+      displayErrorMessage("login-error", "Incorrect password.");
+      let ema = document.getElementById("login-email");
+      let pass = document.getElementById("login-password");
+      ema.value = "";
+      pass.value = "";
+    }
+  } else {
+    displayErrorMessage("login-error", "Email not found.");
+    let ema = document.getElementById("login-email");
+    let pass = document.getElementById("login-password");
+    ema.value = "";
+    pass.value = "";
+  }
+}
+
+function signUp() {
+  let username = document.getElementById("signup-username").value;
+  let email = document.getElementById("signup-email").value;
+  let password = document.getElementById("signup-password").value;
+
+  let storedUser = localStorage.getItem(email);
+  if (storedUser) {
+    displayErrorMessage("signup-error", "Email already in use.");
+    let use = document.getElementById("signup-username");
+    let ema = document.getElementById("signup-email");
+    let pass = document.getElementById("signup-password");
+    use.value = "";
+    ema.value = "";
+    pass.value = "";
+  } else {
+    let user = {
+      username: username,
+      password: password,
+    };
+    localStorage.setItem(email, JSON.stringify(user));
+    showSuccessMessage("Sign up successful!");
+    let use = document.getElementById("signup-username");
+    let ema = document.getElementById("signup-email");
+    let pass = document.getElementById("signup-password");
+    use.value = "";
+    ema.value = "";
+    pass.value = "";
+    window.location.href = "./home.html";
+    document.getElementById("signup-error").style.display = "none"; // Hide error message
+  }
+}
+
+function displayErrorMessage(elementId, message) {
+  let errorElement = document.getElementById(elementId);
+  errorElement.innerText = message;
+  errorElement.style.display = "block";
+  setTimeout(function () {
+    errorElement.style.display = "none";
+  }, 3000);
+}
+
+function showSuccessMessage(message) {
+  let successElement = document.getElementById("success-message");
+  successElement.innerText = message;
+  successElement.style.display = "block";
+  setTimeout(function () {
+    successElement.style.display = "none";
+  }, 3000);
+}
