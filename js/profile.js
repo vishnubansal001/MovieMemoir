2650e142161a31ac0ca157cf4fa509b3c9c6e0ca
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-10-26 17:36:35 +0530

    new:added the styling for the profile page cards | delete functionality

--- js/profile.js
+++ js/profile.js
@@ -23,13 +23,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   movies.forEach((movie, index) => {
     const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
+    cardElement.classList.add("basic-card");
+    cardElement.classList.add("basic-card-dark");
     cardElement.innerHTML = `
-            <h2>${movie.title}</h2>
-            <p>${movie.review}</p>
+            <div class="card-content">
+              <h2>${movie.title}</h2>
+              <p>${movie.review}</p>
+            </div>
+            <div class="card-link">
+              <a href="#" class="update-button" title="Update"><span>Update</span></a>
+            </div>
+            <div class="card-link">
+              <a href="#" class="delete-button" title="Delete"><span>Delete</span></a>
+            </div>
         `;
-    cardElement.addEventListener("click", function () {
+    const updateButton = cardElement.querySelector(".update-button");
+
+    // Add an event listener to the update button
+    updateButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default behavior of the link
       redirectToCardPage(index);
+    });
+
+    const deleteButton = cardElement.querySelector(".delete-button");
+
+    // Add an event listener to the update button
+    deleteButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default behavior of the link
+      deleteItemByIndex("movies", index);
     });
     cardsElement.appendChild(cardElement);
   });
@@ -37,4 +58,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function redirectToCardPage(index) {
   window.location.href = `card.html?id=${index}&cardName=update`;
+}
+
+function deleteItemByIndex(arrayKey, index) {
+  // Retrieve the array from localStorage
+  let items = JSON.parse(localStorage.getItem(arrayKey)) || [];
+
+  // Check if the index is valid
+  if (index >= 0 && index < items.length) {
+    // Use splice to remove the item at the specified index
+    items.splice(index, 1);
+
+    // Update the array in localStorage
+    localStorage.setItem(arrayKey, JSON.stringify(items));
+
+    window.location.href = "profile.html";
+    console.log(`Item at index ${index} deleted successfully.`);
+  } else {
+    console.log("Invalid index.");
+  }
 }
